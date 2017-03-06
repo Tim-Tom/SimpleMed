4feffb1ffe7cbff6b3a8b514263edafe19993e0a
Tim Bollman <tbollman@kevdenti.com>
2017-03-06 10:36:36 -0800

    Make a simple transformer for form input.

--- lib/SimpleMed/Request/UrlEncoded.pm
+++ lib/SimpleMed/Request/UrlEncoded.pm
@@ -7,8 +7,24 @@ use List::Util qw(pairmap);
 
 sub parse {
   my $content = shift;
-  my %parsed = pairmap { $a, ($b eq '') ? undef : $b } parse_urlencoded($content);
-  return \%parsed;
+  my $parsed = {};
+  # As a special thing, we split keys on dashes and make them into arbitrarily complex
+  # objects. Since I wasn't going to use dashes in keys otherwise, this doesn't reduce my
+  # ability to just chain things through the process, but allows me to format the data
+  # nicely.
+  pairmap {
+    my @path = split('-', $a);
+    my $slot = \$parsed;
+    for my $key (@path) {
+      if ($key =~ /^\d+$/a) {
+        $slot = \$$slot->[$key];
+      } else {
+        $slot = \$$slot->{$key};
+      }
+    }
+    $$slot = ($b eq '') ? undef : $b;
+  } parse_urlencoded($content);
+  return $parsed;
 }
 
 1;
